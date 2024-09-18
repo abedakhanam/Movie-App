bd43e273ba14c3920657f8ac5e8cb6aec67fd315
Abeda Khanam <abeda2152@gmail.com>
2024-09-18 16:49:53 +0600

    ..

---
+++ frontend/src/store/rootReducer.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+// Add your reducers here
+const rootReducer = combineReducers({
+  // movies: moviesReducer,
+  // user: userReducer,
+});
+
+export default rootReducer;
